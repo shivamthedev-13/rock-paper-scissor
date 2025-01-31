72240e867826abf470f756231d666c0f7c989556
shivamthedev-13 <shivamthedev13@gmail.com>
2025-01-31 23:45:48 +0530

    chore: add score feature

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ const rockBtnEl = document.querySelector(".rock_btn");
 const paperBtnEL = document.querySelector(".paper_btn");
 const scissorBtnEL = document.querySelector(".scissor_btn");
 const displayEl = document.querySelector("#display");
+const scoreDisplayEl = document.querySelector("#score_display");
+const resetBtnEl = document.querySelector(".reset_btn");
+
+const score = {
+  wins: 0,
+  looses: 0,
+  ties: 0,
+};
+
+resetBtnEl.onclick = function () {
+  score.wins = 0;
+  score.looses = 0;
+  score.ties = 0;
+
+  scoreDisplayEl.querySelector("#win_score").textContent = score.wins;
+  scoreDisplayEl.querySelector("#lose_score").textContent = score.looses;
+  scoreDisplayEl.querySelector("#tie_score").textContent = score.ties;
+};
 
 rockBtnEl.onclick = function () {
   startGame("Rock");
@@ -19,6 +37,7 @@ function startGame(userChoice) {
   const computerChoice = getComputerChoice();
   const result = getResult(userChoice, computerChoice);
   displayResult(result, userChoice, computerChoice);
+  updateScore(result);
 }
 
 function getComputerChoice() {
@@ -50,4 +69,18 @@ function getResult(userChoice, computerChoice) {
 function displayResult(result, userChoice, computerChoice) {
   const resultText = `You chose ${userChoice}, Computer Chose ${computerChoice}, ${result}`;
   displayEl.textContent = resultText;
+}
+
+function updateScore(result) {
+  if (result === "You Win!") {
+    score.wins++;
+  } else if (result === "Computer Win!") {
+    score.looses++;
+  } else if (result === "Tie") {
+    score.ties++;
+  }
+
+  scoreDisplayEl.querySelector("#win_score").textContent = score.wins;
+  scoreDisplayEl.querySelector("#lose_score").textContent = score.looses;
+  scoreDisplayEl.querySelector("#tie_score").textContent = score.ties;
 }
